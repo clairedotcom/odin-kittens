662a65be98d40b617e5e5efa1ef4d5e19f9d7f9d
clairedotcom <claireacarson@gmail.com>
2022-03-03 13:11:46 -0500

    Add destroy action to controller and add corresponsing link to views

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -35,6 +35,13 @@ class KittensController < ApplicationController
     end
   end
 
+  def destroy
+    @kitten = Kitten.find([params[:id]])
+    @kitten.destroy
+
+    redirect_to root_path
+  end
+
   private
 
   def kitten_params
