7479a9fc57554fa5416bbb3926961263b2b439db
clairedotcom <claireacarson@gmail.com>
2022-03-03 12:38:56 -0500

    Add controller actions and create corresponding views

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,5 +1,43 @@
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
+  end
 
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to @kitten
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
